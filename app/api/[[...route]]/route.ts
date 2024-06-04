871f845a089770eddb0320de2b79505fa1e85007
Cyltcc <cyltcc212624@gmail.com>
2024-06-04 08:37:59 +0800

    feat: add

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
@@ -5,11 +7,26 @@ export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
-  return c.json({
-    message: "Hello Next.js!",
-  });
-});
+app
+  .get("/hello", (c) => {
+    return c.json({
+      message: "Hello Next.js!",
+    });
+  })
+  .post(
+    "/",
+    zValidator(
+      "json",
+      z.object({
+        name: z.string(),
+        userId: z.number(),
+      })
+    ),
+    (c) => {
+      const { name, userId } = c.req.valid("json");
+      return c.json({});
+    }
+  );
 
 export const GET = handle(app);
 export const POST = handle(app);
